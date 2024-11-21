25f302225864fd2a4953a325ba4782484440c95d
Mel Hew <meleniehew@gmail.com>
2024-11-21 16:37:45 +1300

    joining contacts table with occupations

--- server/routes/contacts.ts
+++ server/routes/contacts.ts
@@ -7,11 +7,27 @@ const router = Router()
 router.get('/', async (req, res) => {
   try {
     const contacts = await db.getAllContacts()
+    console.log(contacts)
     res.json({ contacts: contacts.map((contact) => contact.name) })
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Something went wrong' })
   }
+
 })
 
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = Number(req.params.id)
+    const data = await db.byId(id)
+    console.log(data)
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
+})
+
+
 export default router
+
