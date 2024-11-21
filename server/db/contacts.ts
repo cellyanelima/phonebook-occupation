095eaba4a479e865a4dce714e29af32cc425b3a2
Cellyane Lima <cellyane@gmail.com>
2024-11-21 17:21:12 +1300

    getAllContacts, getContactById, updateContact

--- server/db/contacts.ts
+++ server/db/contacts.ts
@@ -1,7 +1,32 @@
 import connection from './connection.ts'
-import { Contact } from '../../models/Contact.ts'
+import { Contact, ContactWithOccupation } from '../../models/Contact.ts'
 
-export async function getAllContacts(db = connection): Promise<Contact[]> {
-  const contacts = await db('contacts').select('*')
-  return contacts as Contact[]
+export async function getAllContacts(
+  id: number,
+): Promise<ContactWithOccupation[]> {
+  const contacts = await connection('contacts')
+    .join('occupations', 'contacts.occupation_id', 'occupations.id')
+    .select('contacts.*', 'occupations.name as occupationName')
+    .where('contacts.id', id)
+  return contacts as ContactWithOccupation[]
+}
+
+export async function getContactById(id: number): Promise<Contact> {
+  //console.log(connection('events').where({ id }).select('*').first().toString())
+  const contact = await connection('contacts')
+    .where({ id })
+    .select('id', 'occupation_Id as occupationId', 'name', 'phone', 'email')
+    .first()
+  return contact as Contact
+}
+
+export async function updateContact(updatedContact: Contact): Promise<void> {
+  const { id, occupationId, name, phone, email } = updatedContact
+
+  await connection('contacts').where({ id }).update({
+    occupation_Id: occupationId,
+    name,
+    phone,
+    email,
+  })
 }
--- server/routes/contacts.ts
+++ server/routes/contacts.ts
@@ -7,10 +7,43 @@ const router = Router()
 router.get('/', async (req, res) => {
   try {
     const contacts = await db.getAllContacts()
-    res.json({ contacts: contacts.map((contact) => contact.name) })
+    res.json(contacts)
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = Number(req.params.id)
+
+    const contact = await db.getContactById(id)
+    if (!contact) {
+      return res.status(404).json({ error: 'Contact not found' })
+    }
+    res.json(contact)
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const id = Number(req.body.id)
+    const occupationId = Number(req.body.occupationId)
+    const { name, phone, email } = req.body
+
+    await db.updateContact({
+      id,
+      occupationId: Number(occupationId),
+      name,
+      phone,
+      email,
+    })
+    res.sendStatus(204)
+  } catch (e) {
+    next(e)
   }
 })
 
