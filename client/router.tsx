4818fe82ed902e9dc5866fa8808c9f83f8d997dd
Cellyane Lima <cellyane@gmail.com>
2024-11-22 22:29:13 +1300

    Add new hooks and components

--- client/router.tsx
+++ client/router.tsx
@@ -2,19 +2,22 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import Layout from './components/Layout.tsx'
 import ContactsList from './components/ContactsList.tsx'
-
-
+//import EditOccupation from './components/EditOccupation.tsx'
+//import EditEvent from './components/EditContact.tsx'
+//import NewContact from './components/NewContact.tsx'
+//import NewOccupation from './components/NewOccupation.tsx'
 
 const routes = createRoutesFromElements(
-<Route path="/" element={<Layout />}>
-  <Route index element={<ContactsList />} />
-  <Route path="/contacts" element={<ContactsList />} />
-  
-
-</Route>
-
+  <Route path="/" element={<Layout />}>
+    <Route index element={<ContactsList />} />
+    <Route path="/contacts" element={<ContactsList />} />
+    {/*<Route path="/contacts/:id/edit" element={<EditEvent />} />
+    <Route path="/contacts/add/new" element={<NewContact />} />
+    <Route path="/occupations/:id/edit" element={<EditOccupation />} />
+    <Route path="/occupations/add/new" element={<NewOccupation />} />*/}
+  </Route>,
 )
-  
+
 const router = createBrowserRouter(routes)
 
 export default function App() {
